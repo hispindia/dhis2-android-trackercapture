998856c6a116536117cafee15f4e232e0589cb17
ahmed ifhaam <ahmedifhaam1@gmail.com>
2018-09-07 03:34:34 +0530

    Replaced google map with osmdroid

--- app/src/main/java/org/hisp/dhis/android/trackercapture/activities/MapActivity.java
+++ app/src/main/java/org/hisp/dhis/android/trackercapture/activities/MapActivity.java
@@ -1,22 +1,21 @@
 package org.hisp.dhis.android.trackercapture.activities;
 
 import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 
 
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
-
 import org.hisp.dhis.android.sdk.persistence.models.BaseValue;
 import org.hisp.dhis.android.sdk.persistence.models.TrackedEntityAttributeValue;
 import org.hisp.dhis.android.trackercapture.R;
 import org.hisp.dhis.client.sdk.ui.activities.BaseActivity;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.ItemizedIconOverlay;
+import org.osmdroid.views.overlay.OverlayItem;
 
 import java.util.ArrayList;
 
@@ -24,45 +23,66 @@ import static org.hisp.dhis.android.sdk.ui.adapters.rows.dataentry.AbsDatePicker
 import static org.hisp.dhis.android.sdk.ui.adapters.rows.dataentry.QuestionCoordinatesRow.UNDEFINED;
 
 
-public class MapActivity extends BaseActivity implements OnMapReadyCallback{
-    private GoogleMap mMap;
+public class MapActivity extends BaseActivity{
+    MapView map = null;
     public static String ATTRIBUTE_COORDINATES = "coordinate_attributes_extra";
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
-        SupportMapFragment mapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        map = (MapView) findViewById(R.id.map);
+        map.setTileSource(TileSourceFactory.MAPNIK);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+            map.setHasTransientState(true);
+        }
+        map.setBuiltInZoomControls(true);
+        map.setMultiTouchControls(true);
+
+        ArrayList<OverlayItem> items = new ArrayList<>();
+
+        ArrayList<TrackedEntityAttributeValue> values =
+        (ArrayList<TrackedEntityAttributeValue>) getIntent().getExtras()
+         .get(ATTRIBUTE_COORDINATES);
+        GeoPoint camera = null;
+         if(values!=null){
+
+                 for(TrackedEntityAttributeValue value :values){
+            if(!getLatitudeFromValue(value).equals("")) {
+                    GeoPoint temp = new GeoPoint(
+                                    Double.parseDouble(getLatitudeFromValue(value)),
+                                    Double.parseDouble(getLongitudeFromValue(value)));
+                    items.add(new OverlayItem("","",temp));
+                    camera =temp;
+                }
+             }
+
+         }
+        ItemizedIconOverlay<OverlayItem> mOverlay = new ItemizedIconOverlay<OverlayItem>(getApplicationContext(),items,
+                new ItemizedIconOverlay.OnItemGestureListener<OverlayItem>() {
+                    @Override
+                    public boolean onItemSingleTapUp(int index, OverlayItem item) {
+                        return true;
+                    }
+
+                    @Override
+                    public boolean onItemLongPress(int index, OverlayItem item) {
+                        return false;
+                    }
+                });
+
+
+
+            mOverlay.setFocus(mOverlay.getItem(0));
+         map.getOverlays().add(mOverlay);
+
+
     }
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
-        ArrayList<TrackedEntityAttributeValue> values =
-                (ArrayList<TrackedEntityAttributeValue>) getIntent().getExtras()
-                        .get(ATTRIBUTE_COORDINATES);
-        if(values!=null){
-            LatLng camera = null;
-            for(TrackedEntityAttributeValue value :values){
-                if(!getLatitudeFromValue(value).equals("")) {
-                    LatLng temp = new LatLng(
-                            Double.parseDouble(getLatitudeFromValue(value)),
-                            Double.parseDouble(getLongitudeFromValue(value)));
-                    mMap.addMarker(new MarkerOptions().position(temp));
-                    camera =temp;
-                }
-            }
-            if(camera!=null)mMap.moveCamera(CameraUpdateFactory.newLatLng(camera));
-            
-        }
+    public void onResume(){
+        super.onResume();
 
-
-
-//        LatLng sydney = new LatLng(-34,151);
-//        mMap.addMarker(new MarkerOptions().position(sydney).title("Synd"));
-//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
-
 
     public static String getLatitudeFromValue(BaseValue baseValue) {
         if(baseValue == null || baseValue.getValue() == null)
