1398d38e9dcac509a7ae1c69744d1de7ae081806
Sourabh Bhardwaj <bsourabh191@gmail.com>
2018-09-18 12:23:02 +0530

    current location marker & programstageid check for manual id attribute

--- app/src/main/java/org/hisp/dhis/android/trackercapture/activities/MapActivity.java
+++ app/src/main/java/org/hisp/dhis/android/trackercapture/activities/MapActivity.java
@@ -5,18 +5,19 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
+
+import org.osmdroid.util.BoundingBox;
 import android.view.View;
-
-
+import java.util.List;
 import org.hisp.dhis.android.sdk.controllers.GpsController;
 import org.hisp.dhis.android.sdk.controllers.tracker.TrackerController;
 import org.hisp.dhis.android.sdk.persistence.models.BaseValue;
 import org.hisp.dhis.android.sdk.persistence.models.DataValue;
+import org.hisp.dhis.android.sdk.persistence.models.Event;
 import org.hisp.dhis.android.sdk.persistence.models.TrackedEntityAttributeValue;
 import org.hisp.dhis.android.trackercapture.R;
 import org.hisp.dhis.client.sdk.ui.activities.BaseActivity;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
-import org.osmdroid.util.BoundingBox;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 import org.osmdroid.views.overlay.ItemizedIconOverlay;
@@ -24,7 +25,6 @@ import org.osmdroid.views.overlay.Marker;
 import org.osmdroid.views.overlay.OverlayItem;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import static org.hisp.dhis.android.sdk.ui.adapters.rows.dataentry.AbsDatePickerRow.EMPTY_FIELD;
 import static org.hisp.dhis.android.sdk.ui.adapters.rows.dataentry.QuestionCoordinatesRow.UNDEFINED;
@@ -35,6 +35,7 @@ public class MapActivity extends BaseActivity{
     public static String ATTRIBUTE_COORDINATES = "coordinate_attributes_extra";
     public static final String DATAELEMENT_COORDINATES = "data_element_coordinates:extra";
     public static final String MANUAL_ID_ATTRIBUTE = "I7OncVzPZKS";
+    public static final String EXPOSURE_ID_ATTRIBUTE = "k4gkDsoA1Tp";
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,33 +49,49 @@ public class MapActivity extends BaseActivity{
         map.setBuiltInZoomControls(true);
         map.setMultiTouchControls(true);
 
+
         List<GeoPoint> pointsforBoundingBox = new ArrayList<>();
+
         ArrayList<DataValue> values =
-        (ArrayList<DataValue>) getIntent().getExtras()
-         .get(ATTRIBUTE_COORDINATES);
-         if(values!=null){
-
-                 for(DataValue value :values){
-                     String manualId =
-                             TrackerController.getTrackedEntityAttributeValue(
-                                     MANUAL_ID_ATTRIBUTE,TrackerController.getEvent(value.getLocalEventId()).getTrackedEntityInstance()
-                             ).getValue();
-                        if(!getLatitudeFromValue(value).equals("")) {
-                                Marker marker =  new Marker(map);
-                                marker.setIcon( ContextCompat.getDrawable(getApplicationContext(),R.drawable.lastknown_location_icon));
-
-                                GeoPoint tempGeo = new GeoPoint(
-                                                Double.parseDouble(getLatitudeFromValue(value)),
-                                                Double.parseDouble(getLongitudeFromValue(value)));
-                                marker.setPosition(tempGeo);
-                                marker.setTitle(manualId);
-                                map.getOverlays().add(marker);
-                                pointsforBoundingBox.add(tempGeo);
-
-                            }
+                (ArrayList<DataValue>) getIntent().getExtras()
+                        .get(ATTRIBUTE_COORDINATES);
+        if(values!=null){
+            String manualId="";
+            for(DataValue value :values){
+                Event ev_=TrackerController.getEvent(value.getLocalEventId());
+                if(ev_.getProgramStageId().equals("DbsGMk0zLxr")||ev_.getProgramStageId().equals("R8zfsjiFerK"))
+                {
+                    manualId =
+                            TrackerController.getTrackedEntityAttributeValue(
+                                    EXPOSURE_ID_ATTRIBUTE,TrackerController.getEvent(value.getLocalEventId()).getTrackedEntityInstance()
+                            ).getValue();
+                }
+                else
+                {
+                    manualId =
+                            TrackerController.getTrackedEntityAttributeValue(
+                                    MANUAL_ID_ATTRIBUTE,TrackerController.getEvent(value.getLocalEventId()).getTrackedEntityInstance()
+                            ).getValue();
                 }
 
-         }
+
+                if(!getLatitudeFromValue(value).equals("")) {
+                    Marker marker =  new Marker(map);
+                    marker.setIcon( ContextCompat.getDrawable(getApplicationContext(),R.drawable.lastknown_location_icon));
+
+                    GeoPoint tempGeo = new GeoPoint(
+                            Double.parseDouble(getLatitudeFromValue(value)),
+                            Double.parseDouble(getLongitudeFromValue(value)));
+                    marker.setPosition(tempGeo);
+                    marker.setTitle(manualId);
+                    map.getOverlays().add(marker);
+                    pointsforBoundingBox.add(tempGeo);
+//                    map.getController().animateTo(marker.getPosition());
+
+                }
+            }
+
+        }
 
         ArrayList<DataValue> dataValues =
                 (ArrayList<DataValue>) getIntent().getExtras()
@@ -97,6 +114,8 @@ public class MapActivity extends BaseActivity{
                     marker.setTitle(manualId);
                     map.getOverlays().add(marker);
                     pointsforBoundingBox.add(tempGeo);
+//                    map.getController().animateTo(marker.getPosition());
+
                 }
             }
 
@@ -106,21 +125,20 @@ public class MapActivity extends BaseActivity{
         marker.setIcon(ContextCompat.getDrawable(getApplicationContext(),R.drawable.current_location_icon));
         GeoPoint tempGeo = new GeoPoint(GpsController.getLocation().getLatitude(),GpsController.getLocation().getLongitude());
         marker.setPosition(tempGeo);
+        marker.setTitle("Current Location");
         map.getOverlays().add(marker);
-
 
 
         pointsforBoundingBox.add(tempGeo);
         BoundingBox bb = BoundingBox.fromGeoPoints(pointsforBoundingBox);
-//        map.zoomToBoundingBox(bb,false);
-//        map.getController().zoomToSpan(tempGeo.getLatitude(),tempGeo.getLongitude());
-//        map.setTilesScaledToDpi(true);
+        map.zoomToBoundingBox(bb,false);
+        map.getController().zoomToSpan(tempGeo.getLatitude(),tempGeo.getLongitude());
+        map.setTilesScaledToDpi(true);
         map.getController().zoomTo(9);
         map.getController().animateTo(tempGeo);
 
-
-
     }
+
 
     @Override
     public void onResume(){
